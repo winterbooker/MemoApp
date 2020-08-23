db6f0d13a7f3e176aca44ff58b385b1858c3984a
山下拓 <yamashitataku@taku.local>
2020-08-23 17:40:57 +0900

    Install react-navigation

--- src/screens/MemoListScreen.tsx
+++ src/screens/MemoListScreen.tsx
@@ -5,11 +5,12 @@ import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <MemoList />
-        <CircleButton name="plus" />
+        <MemoList navigation={this.props.navigation} />
+        <CircleButton name="plus" onPress={() => { this.props.navigation.navigate('MemoEdit'); }} />
       </View>
     );
   }
@@ -19,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
+    backgroundColor: '#fffdf6'
   }
 });
 
