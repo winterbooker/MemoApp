6068de045c5d4ba5fb1d484d0052c780504e72f6
山下拓 <yamashitataku@taku.local>
2020-08-23 14:09:49 +0900

    Create Login and Signup Screen

--- App.tsx
+++ App.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoEditScreen />
+      <LoginScreen />
     </View>
   );
 }
