6227beb962d02a2ac2426eaeba7255d4b3594713
山下拓 <yamashitataku@taku.local>
2020-08-24 11:47:19 +0900

    Singup and Login with firebase

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from 'firebase';
 
 
 class LoginScreen extends React.Component {
@@ -9,9 +10,15 @@ class LoginScreen extends React.Component {
   }
 
   handleSubmit() {
-
-    // this.props.navigation.navigate('Home')
-  }
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((result) => {
+        console.log('success!', result.user);
+        this.props.navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log('error!', error);
+  });
+}
 
 
   render() {
