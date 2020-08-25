f74fe8304f8dba56896af089020588d3fa79277a
山下拓 <yamashitataku@taku.local>
2020-08-25 23:15:12 +0900

    Complete edit screen

--- src/screens/MemoDetailScreen.tsx
+++ src/screens/MemoDetailScreen.tsx
@@ -23,6 +23,10 @@ class MemoDetailScreen extends React.Component {
     this.setState({ memo: params.memo });
   }
 
+  returnMemo(memo) {
+    this.setState({ memo });
+  }
+
   render() {
     const { memo } = this.state;
 
@@ -49,7 +53,7 @@ class MemoDetailScreen extends React.Component {
           name="pencil"
           color="white"
           style={styles.editButton}
-          onPress={() => { this.props.navigation.navigate('MemoEdit', { memo }); }} 
+          onPress={() => { this.props.navigation.navigate('MemoEdit', { memo, returnMemo: this.returnMemo.bind(this) }); }}
         />
       </View>
     );
--- src/screens/MemoEditScreen.tsx
+++ src/screens/MemoEditScreen.tsx
@@ -15,19 +15,27 @@ class MemoEditScreen extends React.Component {
     const { params } = this.props.navigation.state;
     this.setState({
       body: params.memo.body,
-      key: params.memo.key
+      key: params.memo.key,
     });
   }
 
   handlePress() {
     const { currentUser } = firebase.auth();
     const db = firebase.firestore();
+    const newDate = firebase.firestore.Timestamp.now();
     db.collection(`users/${currentUser.uid}/memos`).doc(this.state.key)
       .update({
         body: this.state.body,
+        createdOn: newDate,
       })
       .then(() => {
-        console.log('success!');
+        const { navigation } = this.props;
+        navigation.state.params.returnMemo({
+          body: this.state.body,
+          key: this.state.key,
+          createdOn: newDate,
+        });
+        navigation.goBack();
       })
       .catch((error) => {
         console.log(error);
