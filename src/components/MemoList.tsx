96752af40f735cd7efb58c36f00d10e0e4e17c17
山下拓 <yamashitataku@taku.local>
2020-08-25 22:00:19 +0900

    Save change on firestore

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, FlatList } from 'react-native';
 
+// FirebaseのTimestampを一般的な日付文字列にする関数
+const dateString = (date) => {
+  // 存在しない場合は空文字列を返すと安全です
+  if (date == null) { return ''; }
+  // firebaseのTimestamp型をDate型に変換する
+  const dateObject = date.toDate();
+  // Dateオブジェクトを文字列に変換する
+  return dateObject.toISOString().split('T')[0];
+};
+
 class MemoList extends React.Component {
   renderMemo({ item }) {
     console.log(item);
     return (
-      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
+      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
         <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>{item.body}</Text>
-          <Text style={styles.memoDate}>20/8/21</Text>
+          <Text style={styles.memoTitle}>{item.body.substring(0, 10)}</Text>
+          <Text style={styles.memoDate}>{dateString(item.createdOn)}</Text>
         </View>
       </TouchableHighlight>
     );
--- src/screens/MemoDetailScreen.tsx
+++ src/screens/MemoDetailScreen.tsx
@@ -3,26 +3,54 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import CircleButton from '../elements/CircleButton';
 
+// FirebaseのTimestampを一般的な日付文字列にする関数
+const dateString = (date) => {
+  // 存在しない場合は空文字列を返すと安全です
+  if (date == null) { return ''; }
+  // firebaseのTimestamp型をDate型に変換する
+  const dateObject = date.toDate();
+  // Dateオブジェクトを文字列に変換する
+  return dateObject.toISOString().split('T')[0];
+};
+
 class MemoDetailScreen extends React.Component {
+  state = {
+    memo: {},
+  }
+
+  UNSAFE_componentWillMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({ memo: params.memo });
+  }
+
   render() {
+    const { memo } = this.state;
+
+    if (memo == null) { return null; }
+
     return (
       <View style={styles.container}>
         <View>
           <View style={styles.memoHeader}>
             <View>
-              <Text style={styles.memoHeaderTitle}>講座のアイデア</Text>
-              <Text style={styles.memoHeaderDate}>20/8/21</Text>
+              <Text style={styles.memoHeaderTitle}>{memo.body.substring(0, 10)}</Text>
+              <Text style={styles.memoHeaderDate}>{dateString(memo.createdOn)}</Text>
             </View>
           </View>
         </View>
 
         <View style={styles.memoContent}>
-          <Text>
-            講座のアイデアです。
+          <Text style={styles.memoBody}>
+            {memo.body}
           </Text>
         </View>
 
-        <CircleButton name="pencil" color="white" style={styles.editButton} onPress={() => { this.props.navigation.navigate('MemoEdit') }} />
+        <CircleButton
+          name="pencil"
+          color="white"
+          style={styles.editButton}
+          onPress={() => { this.props.navigation.navigate('MemoEdit', { memo }); }} 
+        />
       </View>
     );
   }
@@ -56,6 +84,10 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
     backgroundColor: '#fff',
     flex: 1,
+  },
+  memoBody: {
+    lineHeight: 22,
+    fontSize: 15,
   },
   editButton: {
     top: 75,
