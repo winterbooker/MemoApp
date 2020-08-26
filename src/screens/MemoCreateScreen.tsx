79d25d0578157d47e904af88f2b226cc7a3d24ea
山下拓 <yamashitataku@taku.local>
2020-08-26 11:05:11 +0900

    Fix button position bug

--- src/screens/MemoCreateScreen.tsx
+++ src/screens/MemoCreateScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 import firebase from 'firebase';
 
 import CircleButton from '../elements/CircleButton';
@@ -25,7 +25,7 @@ class MemoCreateScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -34,7 +34,7 @@ class MemoCreateScreen extends React.Component {
           textAlignVertical="top"
         />
         <CircleButton name="check" onPress={this.handlePress.bind(this)}/>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
